d7ac8350eb437bf2f9c396ace2ce2046479d2851
“pedrokp” <pedrokp@gmail.com>
2022-01-17 18:44:07 -0300

    Meu primeiro commit

---
+++ build/precache-manifest.19ca9b8188217514cb56d712374fba67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b3c461d807a0cb5d57fbf98801cbba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5abc36a3cc3f584a45d",
+    "url": "/static/css/main.ca85a575.chunk.css"
+  },
+  {
+    "revision": "c09b27350b817e933b9f",
+    "url": "/static/js/2.727d54b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.727d54b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5abc36a3cc3f584a45d",
+    "url": "/static/js/main.9f6c42ef.chunk.js"
+  },
+  {
+    "revision": "0481832ad9d84c14443f",
+    "url": "/static/js/runtime-main.43ed6a93.js"
+  }
+]);
